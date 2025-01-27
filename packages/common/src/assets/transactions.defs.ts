69e9acc3cb890617542bf3febc99f7a1dfb36ff5
Tang Bo Hao <tech@btang.cn>
2025-01-27 12:02:41 +0700

    feat: add init-agent-account to defs

---
+++ packages/common/src/assets/transactions.defs.ts
@@ -0,0 +1,10 @@
+// Source:
+//
+// This file contains the definitions of the Cadence transactins used in the plugin.
+// The transactions are defined as strings and exported as a dictionary.
+
+import initAgentAccount from "./cadence/transactions/init-agent-account.cdc?raw";
+
+export const transactions = {
+    initAgentAccount,
+};
