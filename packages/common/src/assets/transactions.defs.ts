73f758615a5992fa9f0160177614b691ac6d2fd1
Tang Bo Hao <tech@btang.cn>
2025-01-29 23:31:13 +0800

    fix: common build

--- packages/common/src/assets/transactions.defs.ts
+++ packages/common/src/assets/transactions.defs.ts
@@ -6,9 +6,9 @@
 import initAgentAccount from "./cadence/transactions/init_agent_account.cdc?raw";
 // Account Pool related transactions
 import acctPoolCreateChildAccount from "./cadence/transactions/account-pool/create_child.cdc?raw";
-import acctPoolEVMTransferERC20 from "./cadence/transactions/account-pool/evm/transfer_erc20.cdc?raw";
-import acctPoolFlowTokenDynamicTransfer from "./cadence/transactions/account-pool/flow-token/dynamic_vm_transfer.cdc?raw";
-import acctPoolFTGenericTransfer from "./cadence/transactions/account-pool/ft/generic_transfer_with_address.cdc?raw";
+import acctPoolEVMTransferERC20 from "./cadence/transactions/account-pool/evm/transfer_erc20_from.cdc?raw";
+import acctPoolFlowTokenDynamicTransfer from "./cadence/transactions/account-pool/flow-token/dynamic_vm_transfer_from.cdc?raw";
+import acctPoolFTGenericTransfer from "./cadence/transactions/account-pool/ft/generic_transfer_with_address_from.cdc?raw";
 
 export const transactions = {
     initAgentAccount,
